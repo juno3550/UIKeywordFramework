c36af4d83ab49fe6b35ee515cc8e56789aedb35a
juno3550 <78023734+juno3550@users.noreply.github.com>
2021-04-15 01:38:50 +0800

    框架第一层：测试工具层

--- util/email_util.py
+++ util/email_util.py
@@ -7,7 +7,7 @@ def send_mail(attachments_report_name, receiver, subject, content):
     try:
         # 连接邮箱服务器
         # 注意：若使用QQ邮箱，则password为授权码而非邮箱密码；使用其它邮箱则为邮箱密码
-        # encoding设置为GBK，否则中文附件名会乱码
+        # encoding设置为GBK，否则中文附件名会乱码 
         yag = yagmail.SMTP(user="******@163.com", password="******", host="smtp.163.com", encoding='GBK')
 
         # 收件人、标题、正文、附件（若多个收件人或多个附件，则可使用列表）
